4135e0719333850e5c4de0cc328b5487fe8dd3dc
bala-amypo <bala@amypo.in>
2025-12-26 06:44:53 +0000

    User commit

--- src/main/java/com/example/demo/security/UserPrincipal.java
+++ src/main/java/com/example/demo/security/UserPrincipal.java
@@ -36,6 +36,29 @@
 // }
 
 
+// package com.example.demo.security;
+
+// public class UserPrincipal {
+//     private Long id;
+//     private String email;
+//     private String password;
+//     private String role;
+    
+//     public UserPrincipal(Long id, String email, String password, String role) {
+//         this.id = id;
+//         this.email = email;
+//         this.password = password;
+//         this.role = role;
+//     }
+    
+//     public Long getId() { return id; }
+//     public String getEmail() { return email; }
+//     public String getPassword() { return password; }
+//     public String getRole() { return role; }
+//     public String getUsername() { return email; }
+// }
+
+
 package com.example.demo.security;
 
 public class UserPrincipal {

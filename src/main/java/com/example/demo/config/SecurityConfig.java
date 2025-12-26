c3c164bd4a445fce45af35a2b8736a0c2b18a49e
bala-amypo <bala@amypo.in>
2025-12-26 07:45:57 +0000

    User commit

--- src/main/java/com/example/demo/config/SecurityConfig.java
+++ src/main/java/com/example/demo/config/SecurityConfig.java
@@ -1,31 +1,31 @@
-// // package com.example.demo.security;
+package com.example.demo.security;
 
-// // import org.springframework.context.annotation.Bean;
-// // import org.springframework.context.annotation.Configuration;
-// // import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-// // import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-// // import org.springframework.security.config.http.SessionCreationPolicy;
-// // import org.springframework.security.web.SecurityFilterChain;
-// // import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-// // @Configuration
-// // @EnableWebSecurity
-// // public class SecurityConfig {
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
     
-// //     @Bean
-// //     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-// //         http
-// //             .csrf(csrf -> csrf.disable())
-// //             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-// //             .authorizeHttpRequests(auth -> auth
-// //                 .requestMatchers("/auth/**").permitAll()
-// //                 .requestMatchers("/api/**").authenticated()
-// //                 .anyRequest().permitAll()
-// //             );
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .csrf(csrf -> csrf.disable())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/auth/**").permitAll()
+                .requestMatchers("/api/**").authenticated()
+                .anyRequest().permitAll()
+            );
         
-// //         return http.build();
-// //     }
-// // }
+        return http.build();
+    }
+}
 
 
 
